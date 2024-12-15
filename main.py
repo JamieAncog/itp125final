51da090c6a03a83d42550b545ea3debc5f1ae173
Jamie Ancog <jamieancog@gmail.com>
2024-12-15 01:01:20 -0800

    increased performance

--- main.py
+++ main.py
@@ -15,47 +15,40 @@ def gen_md5(password):
     return md5hash
 
 
-def all_combos(letters, n, prefixes, prefix):
+def all_combos(start, letters, n, prefixes, prefix, password_list, hashes):
     if len(prefix) == n:
-        print(prefix)
         prefixes.append(prefix)
-        return
+        for curr_hash in hashes:
+            if gen_md5(prefix) == curr_hash:
+                password_list.append(prefix)
+                curr_time = time.time()
+                elapsed_time = curr_time - start
+
+                passwords_file = open("passwords.txt", "a")
+                passwords_file.write(passwords[-1] + "\n")
+                passwords_file.write(str(elapsed_time) + " seconds to crack\n\n")
+                passwords_file.close()
+
+                del curr_hash
+                return
     else:
         for x in range(len(letters)):
-            all_combos(letters, n, prefixes, prefix + letters[x])
+            all_combos(start, letters, n, prefixes, prefix + letters[x], password_list, hashes)
 
 
-def decrypt_md5(max_length, curr_hash, password_list, hashes):
-    chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-    chars_with_symbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%&*()?"
+def decrypt_md5(max_length, password_list, hashes):
+    chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@!?"
+    start_time = time.time()
 
     length = 1
-    while length <= max_length:
+    while length <= max_length and len(hashes) > 0:
         prefixes = []
-        all_combos(chars, length, prefixes, "")
-        for prefix in prefixes:
-            if gen_md5(prefix) == hashes[curr_hash]:
-                password_list.append(prefix)
-                return
+        all_combos(start_time, chars, length, prefixes, "", password_list, hashes)
         length += 1
 
 
 passwords = []
-for i in range(1):
-    passwords_file = open("passwords.txt", "a")
-    start_time = time.perf_counter()
-
-    decrypt_md5(10, 4, passwords, hashes_list)
-
-    end_time = time.perf_counter()
-    elapsed_time = end_time - start_time
-
-    print(passwords[-1])
-    print(str(elapsed_time) + " seconds to crack\n")
-
-    passwords_file.write(passwords[-1] + "\n")
-    passwords_file.write(str(elapsed_time) + " seconds to crack\n\n")
-    passwords_file.close()
+decrypt_md5(10, passwords, hashes_list)
 
 
 hash_file.close()
