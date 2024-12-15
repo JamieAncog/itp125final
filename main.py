758eb0269d1355173174830802d1f5f03da16770
Jamie Ancog <jamieancog@gmail.com>
2024-12-15 04:10:16 -0800

    first multiprocessing attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import hashlib
 import time
+import multiprocessing
 
 # Open hashes.txt and read each hash into a list
 
@@ -15,36 +16,56 @@ def gen_md5(password):
     return md5hash
 
 
-def all_combos(start, letters, n, prefix, password_list, hashes):
-    for curr_hash in hashes:
-        if gen_md5(prefix) == curr_hash:
-            password_list.append(prefix)
-            curr_time = time.time()
-            elapsed_time = curr_time - start
-
-            passwords_file = open("passwords.txt", "a")
-            passwords_file.write(passwords[-1] + "\n")
-            passwords_file.write(str(elapsed_time) + " seconds to crack\n\n")
-            passwords_file.close()
-
-            del curr_hash
-
+def all_combos(letters, n, prefixes, prefix, curr_hash, password_list, hashes):
     if len(prefix) == n:
-        return
+        prefixes.append(prefix)
+        if gen_md5(prefix) == hashes[curr_hash]:
+            password_list.append(prefix)
+            return
     else:
         for x in range(len(letters)):
-            all_combos(start, letters, n, prefix + letters[x], password_list, hashes)
+            all_combos(letters, n, prefixes, prefix + letters[x], curr_hash, password_list, hashes)
 
 
-def decrypt_md5(max_length, password_list, hashes):
-    chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@!?"
-    start_time = time.time()
+def decrypt_md5(max_length, curr_hash, password_list, hashes):
+    chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    chars_with_symbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%&*()?"
 
-    all_combos(start_time, chars, max_length, "", password_list, hashes)
+    length = 1
+    while length <= max_length:
+        prefixes = []
+        all_combos(chars, length, prefixes, "", curr_hash, password_list, hashes)
+        # print(prefixes)
+        if len(password_list) > 0 and gen_md5(password_list[-1]) == hashes[curr_hash]:
+            return
+        length += 1
 
 
-passwords = []
-decrypt_md5(3, passwords, hashes_list)
+def helper(n):
+    passwords = []
+    passwords_file = open("passwords.txt", "a")
+    start_time = time.perf_counter()
+
+    decrypt_md5(5, n, passwords, hashes_list)
+
+    end_time = time.perf_counter()
+    elapsed_time = end_time - start_time
+
+    print(passwords[-1])
+    print(str(elapsed_time) + " seconds to crack\n\n")
+
+    passwords_file.close()
+
+
+if __name__ == '__main__':
+    processes = []
+    for i in range(5):
+        p = multiprocessing.Process(target=helper, args=(i,))
+        processes.append(p)
+        p.start()
+
+    for process in processes:
+        process.join()
 
 
 hash_file.close()
