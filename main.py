17c3bd69aa5ad1dae3615fbb3f48780efa85c92c
Jamie Ancog <jamieancog@gmail.com>
2024-12-14 20:03:11 -0800

    undo recursive

--- main.py
+++ main.py
@@ -15,15 +15,14 @@ def gen_md5(password):
     return md5hash
 
 
-def all_combos(letters, n, prefixes, prefix, curr_hash, password_list, hashes):
+def all_combos(letters, n, prefixes, prefix):
     if len(prefix) == n:
+        print(prefix)
         prefixes.append(prefix)
-        if gen_md5(prefix) == hashes[curr_hash]:
-            password_list.append(prefix)
-            return
+        return
     else:
         for x in range(len(letters)):
-            all_combos(letters, n, prefixes, prefix + letters[x], curr_hash, password_list, hashes)
+            all_combos(letters, n, prefixes, prefix + letters[x])
 
 
 def decrypt_md5(max_length, curr_hash, password_list, hashes):
@@ -33,28 +32,29 @@ def decrypt_md5(max_length, curr_hash, password_list, hashes):
     length = 1
     while length <= max_length:
         prefixes = []
-        all_combos(chars, length, prefixes, "", curr_hash, password_list, hashes)
-        # print(prefixes)
-        if gen_md5(password_list[-1]) == hashes[curr_hash]:
-            return
+        all_combos(chars, length, prefixes, "")
+        for prefix in prefixes:
+            if gen_md5(prefix) == hashes[curr_hash]:
+                password_list.append(prefix)
+                return
         length += 1
 
 
 passwords = []
-for i in range(4):
+for i in range(1):
     passwords_file = open("passwords.txt", "a")
     start_time = time.perf_counter()
 
-    decrypt_md5(5, i, passwords, hashes_list)
+    decrypt_md5(10, 4, passwords, hashes_list)
 
     end_time = time.perf_counter()
     elapsed_time = end_time - start_time
 
     print(passwords[-1])
-    print(str(elapsed_time) + " seconds to crack\n\n")
+    print(str(elapsed_time) + " seconds to crack\n")
 
-    # passwords_file.write(passwords[-1] + "\n")
-    # passwords_file.write(str(elapsed_time) + " seconds to crack\n\n")
+    passwords_file.write(passwords[-1] + "\n")
+    passwords_file.write(str(elapsed_time) + " seconds to crack\n\n")
     passwords_file.close()
 
 
