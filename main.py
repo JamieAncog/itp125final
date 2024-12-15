f2f2d1c8d079ac1f6fd38f55e4f2f97c1b46badd
Jamie Ancog <jamieancog@gmail.com>
2024-12-15 10:34:57 -0800

    second multiprocessing attempt

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import multiprocessing
 
 hash_file = open("hashes.txt", "r")
 content = hash_file.read()
+hash_file.close()
 hashes_list = content.splitlines()
 
 # Use the hashlib library to generate the md5 hash for a given password
@@ -16,7 +17,7 @@ def gen_md5(password):
     return md5hash
 
 
-def all_combos(letters, n, prefixes, prefix, curr_hash, password_list, hashes):
+def all_combos_rec(letters, n, prefixes, prefix, curr_hash, password_list, hashes):
     if len(prefix) == n:
         prefixes.append(prefix)
         if gen_md5(prefix) == hashes[curr_hash]:
@@ -24,7 +25,16 @@ def all_combos(letters, n, prefixes, prefix, curr_hash, password_list, hashes):
             return
     else:
         for x in range(len(letters)):
-            all_combos(letters, n, prefixes, prefix + letters[x], curr_hash, password_list, hashes)
+            all_combos_rec(letters, n, prefixes, prefix + letters[x], curr_hash, password_list, hashes)
+
+
+def all_combos(letters, n, prefixes, prefix):
+    if len(prefix) == n:
+        prefixes.append(prefix)
+        return
+    else:
+        for x in range(len(letters)):
+            all_combos(letters, n, prefixes, prefix + letters[x])
 
 
 def decrypt_md5(max_length, curr_hash, password_list, hashes):
@@ -33,7 +43,7 @@ def decrypt_md5(max_length, curr_hash, password_list, hashes):
     length = 1
     while length <= max_length:
         prefixes = []
-        all_combos(chars, length, prefixes, "", curr_hash, password_list, hashes)
+        all_combos_rec(chars, length, prefixes, "", curr_hash, password_list, hashes)
         # print(prefixes)
         if len(password_list) > 0 and gen_md5(password_list[-1]) == hashes[curr_hash]:
             return
@@ -55,7 +65,7 @@ def helper(n):
 
     passwords_file.close()
 
-
+'''
 if __name__ == '__main__':
     processes = []
     for i in range(5):
@@ -66,4 +76,33 @@ if __name__ == '__main__':
     for process in processes:
         process.join()
 
-    hash_file.close()
+'''
+
+
+def do_sm():
+    print("Sleeping 1 second...")
+    time.sleep(1)
+    print("Done Sleeping...")
+
+
+def h():
+    my_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@!?"
+    my_prefixes = []
+    all_combos(my_chars, 8, my_prefixes, "")
+
+
+if __name__ == '__main__':
+    st = time.perf_counter()
+
+    processes = []
+    for i in range(1):
+        p = multiprocessing.Process(target=h)
+        processes.append(p)
+        p.start()
+
+    for process in processes:
+        process.join()
+
+    en = time.perf_counter()
+    ela = en - st
+    print(str(ela) + " seconds")
